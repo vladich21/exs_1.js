7506ef8abec21681bd1a2658a3f45272e1dff412
Vlad <vladkoval@gmail.com>
2024-03-07 19:16:31 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+let number = 4.6;
+
+console.log(-4/0);
+console.log('string' * 9);
+
+const person = `5`;
+
+const bool = false;
+
+console.log(something);
---
+++ javaScript/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+console.log('arr' + " - object");
+console.log(4 + +"5");
+
+let incr = 10;
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2+2*2 === 8);
+
+&& оператор И работает только тогда когда 2 или больше подопыдных являются true, если нет тогда false
+|| оператор ИЛИ работает только тогда когда хотя бы один из подопыдных является true
+! обращает значение в отрицание 
+
+const isCheked = false,
+    isClose = false;
+
+console.log(isCheked || !isClose);
+/*Результат true*/ 
+
