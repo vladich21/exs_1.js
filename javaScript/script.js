6573ed4ca342a8549bb808a7f03da4cf17c90cf6
Vlad <vladkoval@gmail.com>
2024-03-12 14:36:25 +0300

    log oper

--- javaScript/script.js
+++ javaScript/script.js
@@ -73,21 +73,78 @@ console.log(hamburger && fries);
 // }
 
 
-
+// ЛОГИЧЕСКИЕ ОПЕРАТОРЫ 
 
 const hamburger = 3;
 const fries = 3;
 const cola = 0; 
 const nuggets = 2;
 
-if (hamburger === 3 && cola ===2 || fries === 3 && nuggets) {
+if (hamburger === 3 && (cola === 2 || fries === 3) && nuggets) {
     console.log('Все довольны!');
 } else {
     console.log('Мы уходим!');
 }
 
-console.log(hamburger === 3 && cola ===2 || fries === 3 && nuggets);
+console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
 
 let johnReport, alexReport, samReport, mariaReport = 'done';
 
 console.log(johnReport || alexReport || samReport || mariaReport);
+
+console.log(!0);
+
+console.log( NaN || 2 || undefined );
+// ответ 2
+// Когда вы используете оператор "или" || в JavaScript, он возвращает первое истинное значение из списка операндов или последний операнд, если ни один из них не является истинным.
+
+console.log( NaN && 2 && undefined );
+// ответ NaN
+// В этом выражении используется оператор "и" && в JavaScript. Он возвращает первое ложное значение из списка операндов или последний операнд, если все операнды истинные.
+
+console.log( 1 && 2 && 3 ); 
+// ответ 3
+console.log( !1 && 2 || !3 );
+// ответ !1
+console.log( 25 || null && !3 );
+// ответ  25
+console.log( NaN || null || !3 || undefined || 5);
+// ответ 5 
+console.log( NaN || null && !3 && undefined || 5);
+// ответ 5 
+console.log( 5 === 5 && 3 > 1 || 5);
+// ответ true
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+
+if (hamburger === 3 && cola || fries === 3 && nuggets) {
+   console.log('Done!')
+}
+//done
+
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+
+if (hamburger || cola || fries === 3 || nuggets) {
+   console.log('Done!')
+}
+//done
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!')
+}
+// false
